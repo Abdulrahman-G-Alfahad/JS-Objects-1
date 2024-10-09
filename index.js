5e7870b353a85743cac268d5f2c3bb6e8cb6739e
unknown <abdulrahman.g.alfahad@gmail.com>
2024-10-09 10:25:09 +0300

    Completed all Tasks

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-//Working with abdulrahman alfahad
+//Working with Abdulrahman Alfahad & Ibrahim AlIbrahim
 
 /*****************************************************************
 Part 1: Understanding Objects
@@ -16,15 +16,17 @@ Task 1) Create an object named 'book' using curly braces {} that includes the in
 Task 2) Access and return the book's title using dot notation and the published year using bracket notation.
 ******************************************************************/
 
-let book={
-    Title:"JavaScript: The Definitive Guide",
-    Author: "David Flanagan",
-    PublishedYear: 2020,
-    Genre: "Programming",
-}
-//console.log(book.Title);
-//console.log(book['PublishedYear']);
+//Task 1
+const book = {
+  Title: "JavaScript: The Definitive Guide",
+  Author: "David Flanagan",
+  PublishedYear: 2020,
+  Genre: "Programming",
+};
 
+//Task 2
+const title = book.Title;
+const publishedYear = book["PublishedYear"];
 
 /*****************************************************************
 Part 2: Modifying Objects
@@ -38,8 +40,14 @@ Task 4) The book has been assigned an ISBN number. Add a property 'ISBN' with a 
 Task 5) Modify the 'publishedYear' to 2021 as a new edition has been released.
 ******************************************************************/
 
-book.pageCount=1096;
-book.ISBN="978-1491952023";
+//Task 3
+book.pageCount = 1096;
+
+//Task 4
+book.ISBN = "978-1491952023";
+
+//Task 5
+book.PublishedYear = 2021;
 
 //console.log(book.pageCount);
 //console.log(book.ISBN);
@@ -57,12 +65,16 @@ Task 7) Add a 'reviews' property to the 'book' object, which will store an array
         Start with one review: {reviewer: "Book Critic", comment: "A comprehensive guide to JavaScript."}.
 ******************************************************************/
 
-book.Author=["David Flanagan","Another Author"];
+//Task 6
+book.Author = ["David Flanagan", "Another Author"];
 //console.log(book.Author)
 
-book.reviews={
-    reviewer: "Book Critic", 
-    comment: "A comprehensive guide to JavaScript."
-}
+//Task 7
+book.reviews = [
+  {
+    reviewer: "Book Critic",
+    comment: "A comprehensive guide to JavaScript.",
+  },
+];
 
-//console.log(book.reviews.comment);
+//console.log(book.reviews[0]);
